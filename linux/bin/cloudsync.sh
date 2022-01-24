b0a6929f447313bd2adb9b212432abab12477ef2
rbr <rbr@arcor.de>
2022-01-24 17:02:22 +0100

    make cloudsync script interactive
    make a dry-run first and then ask run full sync with prune older backups

--- linux/bin/cloudsync.sh
+++ linux/bin/cloudsync.sh
@@ -4,10 +4,10 @@
 
 src_path=$1
 dest_path=$2
-dry_run=$3
-remote_path="ProjectsOneDrive:Backup/Projects.Legulas"
+remote_path="ProjectsOneDrive:Backup/Projects.WS016"
 remote_bak_path="$remote_path/$dest_path-$(date +%F_%W_%a)"
-rclone_sync="rclone sync $dry_run --transfers=20 --checkers=20 -Pv"
+rclone_sync_dry="rclone sync --dry-run --transfers=20 --checkers=20 -Pv"
+rclone_sync="rclone sync --transfers=20 --checkers=20 -Pv"
 rclone_lsd="rclone lsd"
 rclone_prune="rclone prune"
 
@@ -19,7 +19,18 @@ echo "--- source: $src_path"
 echo "--- remote: $remote_path"
 echo "--- backup: $remote_bak_path"
 
-dest_bak_list=$($rclone_lsd $remote_path 2> /dev/null |grep $dest_path)
+echo "--- check sync to remote backup: $latest_bak"
+echo "--- $rclone_sync_dry $src_path $remote_bak_path"
+$rclone_sync_dry $src_path $remote_bak_path 2> /dev/null \
+  && $rclone_lsd $remote_path 2> /dev/null |grep $dest_path| sort -k 2
+echo "run sync ? (n/y):"
+read sync_answer
+
+if [[ "$sync_answer" != "y" ]]; then
+  exit 0
+fi
+
+dest_bak_list=$($rclone_lsd $remote_path 3> /dev/null |grep $dest_path)
 bak_cnt=$(echo "$dest_bak_list" |wc -l)
 echo "--- current backup $dest_path count: $bak_cnt"
 
@@ -34,6 +45,6 @@ fi
 #rclone sync --transfers=20 --checkers=20 -Pv $(srcDir) ProjectsOneDrive:Backup/Projects.Legulas/pktdump-2021-11-04_44_Thu && rclone lsd ProjectsOneDrive:Backup/Projects.Legulas |grep pktdump
 echo "--- sync to remote backup: $latest_bak"
 echo "--- $rclone_sync $src_path $remote_bak_path"
-$rclone_sync $src_path $remote_bak_path 2> /dev/null \
+echo $rclone_sync $src_path $remote_bak_path 2> /dev/null \
   && $rclone_lsd $remote_path 2> /dev/null |grep $dest_path| sort -k 2
 echo "... sync done"
