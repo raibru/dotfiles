975c42ceaddba06269cf62b0aef11212d8849425
rbr <rbr@arcor.de>
2022-01-25 09:56:41 +0100

    purge older backups with cloudsysnc shell script

--- linux/bin/cloudsync.sh
+++ linux/bin/cloudsync.sh
@@ -9,7 +9,7 @@ remote_bak_path="$remote_path/$dest_path-$(date +%F_%W_%a)"
 rclone_sync_dry="rclone sync --dry-run --transfers=20 --checkers=20 -Pv"
 rclone_sync="rclone sync --transfers=20 --checkers=20 -Pv"
 rclone_lsd="rclone lsd"
-rclone_prune="rclone prune"
+rclone_purge="rclone purge"
 
 bak_max_size=7
 
@@ -23,28 +23,33 @@ echo "--- check sync to remote backup: $latest_bak"
 echo "--- $rclone_sync_dry $src_path $remote_bak_path"
 $rclone_sync_dry $src_path $remote_bak_path 2> /dev/null \
   && $rclone_lsd $remote_path 2> /dev/null |grep $dest_path| sort -k 2
-echo "run sync ? (n/y):"
+
+dest_bak_list=$($rclone_lsd $remote_path 3> /dev/null |grep $dest_path)
+bak_cnt=$(echo "$dest_bak_list" |wc -l)
+latest_bak=$(echo "$dest_bak_list" |sort -k 2 |grep $dest_path |head -n1 |tr -s " " " " |cut -d" " -f6)
+
+echo "--- current backup $dest_path count: $bak_cnt from max count: $bak_max_size"
+if [[ bak_cnt -ge back_max_size ]]; then
+  echo "--- will purge latest remote backup: $latest_bak"
+fi
+echo "run purge/backup ? (n/y): [n]"
 read sync_answer
 
 if [[ "$sync_answer" != "y" ]]; then
   exit 0
 fi
 
-dest_bak_list=$($rclone_lsd $remote_path 3> /dev/null |grep $dest_path)
-bak_cnt=$(echo "$dest_bak_list" |wc -l)
-echo "--- current backup $dest_path count: $bak_cnt"
-
 if [[ bak_cnt -ge bak_max_size ]]; then
-  latest_bak=$(echo "$dest_bak_list" |sort -k 2 |grep $dest_path |head -n1 |tr -s " " " " |cut -d" " -f6)
-  echo "--- prune latest remote backup: $latest_bak"
-  prune_path="ProjectsOneDrive:Backup/Projects.Legulas/$latest_bak"
-  echo "$rclone_prune -Pv $prune_path"
-  echo "... prune done"
+  purge_path="$remote_path/$latest_bak"
+  echo "$rclone_purge -Pv $purge_path"
+  $rclone_purge -Pv $purge_path
+  echo "... purge done"
 fi
 
 #rclone sync --transfers=20 --checkers=20 -Pv $(srcDir) ProjectsOneDrive:Backup/Projects.Legulas/pktdump-2021-11-04_44_Thu && rclone lsd ProjectsOneDrive:Backup/Projects.Legulas |grep pktdump
-echo "--- sync to remote backup: $latest_bak"
+echo "--- backup to remote: $latest_bak"
 echo "--- $rclone_sync $src_path $remote_bak_path"
-echo $rclone_sync $src_path $remote_bak_path 2> /dev/null \
+$rclone_sync $src_path $remote_bak_path 2> /dev/null \
   && $rclone_lsd $remote_path 2> /dev/null |grep $dest_path| sort -k 2
-echo "... sync done"
+echo "... backup done"
+
